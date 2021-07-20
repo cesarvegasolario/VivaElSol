801d0a8e34ae581e7afb2be7186f9f4b70878419
g-vega-cl <g.vega.cl@gmail.com>
2021-07-20 17:38:02 -0500

    services

--- src/Servicios/Servicios.js
+++ src/Servicios/Servicios.js
@@ -1,3 +1,5 @@
+import ServicesImage from '../Images/Grupo 19@1X.png';
+import comercioIndustrialImage from '../Images/Grupo 7@1X.png';
 
 const Servicios = () => {
 
@@ -12,7 +14,9 @@ const Servicios = () => {
       padding:'20px',
       justifyContent:'center'
     }}>
-      <p style={{position:'absolute', color: 'black', fontSize:'60px', top:'0px', margin:'0px'}}>Servicios</p>
+      {/* <p style={{position:'absolute', color: 'black', fontSize:'60px', top:'0px', margin:'0px'}}>Servicios</p> */}
+      <img src={ServicesImage} alt="services" />
+      <img src={comercioIndustrialImage} alt="comercio e industrial" style={{position:'absolute', zIndex:'1', marginTop:'230px'}} />
     </div>
     </>
   )
